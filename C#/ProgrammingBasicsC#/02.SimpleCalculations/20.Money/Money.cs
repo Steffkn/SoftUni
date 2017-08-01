7c6a9a6c8645567abbbe250c6fa1883e14e516ba
Steffkn <steff.kn@abv.bg>
2017-08-01 16:43:16 +0300

    Rename: Changed Program.cs to Money.cs in SimpleCalculations

--- C#/ProgrammingBasicsC#/02.SimpleCalculations/20.Money/Money.cs
+++ C#/ProgrammingBasicsC#/02.SimpleCalculations/20.Money/Money.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public class Program
+    public class Money
     {
         const decimal BITCOIN_TO_BGN = 1168m;
         const decimal CHINESE_IOAN_TO_DOLAR = 0.15m;
@@ -13,13 +13,13 @@
         {
             var bitcoinsAmount = int.Parse(Console.ReadLine());
             var chineseIoanAmount = decimal.Parse(Console.ReadLine());
-            var poundage = int.Parse(Console.ReadLine());
+            decimal poundage = decimal.Parse(Console.ReadLine());
 
             decimal moneyAmountInBGN = bitcoinsAmount * BITCOIN_TO_BGN + (chineseIoanAmount * CHINESE_IOAN_TO_DOLAR) * DOLAR_TO_BGN;
             decimal moneyAmountInEUR = moneyAmountInBGN / EUR_TO_BGN;
-            decimal totalAmount = moneyAmountInEUR - (moneyAmountInEUR * 5) / 100;
+            decimal totalAmount = moneyAmountInEUR - (moneyAmountInEUR * poundage) / 100;
 
-            Console.WriteLine(string.Format("{0:F2}", Math.Round(totalAmount, 2)));
+            Console.WriteLine(string.Format("{0:F2}", totalAmount));
         }
     }
 }
