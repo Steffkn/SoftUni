35604713763682eac76028f6e10d9c2cd6c297e6
Steffkn <steff.kn@abv.bg>
2017-11-22 12:19:09 +0200

    Added: HW 05.JavaScriptBlogDesign - Blog using the template we created in HTML and CSS - BlogDesign

---
+++ C#/02.TechModule-09.2017/SoftwareTech/05.JavaScriptBlogDesign/controllers/home.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Article = mongoose.model('Article');
+
+module.exports = {
+  index: (req, res) => {
+      Article.find({}).limit(6).populate('author').then(articles =>{
+            res.render('home/index', {articles: articles});
+            console.log(articles);
+      })
+  }
+};
