37b06885762f149c43e9e138c9d7cf3383bf1806
Steffkn <steff.kn@abv.bg>
2018-02-06 11:53:08 +0200

    Added: Exam section and 25June2017.sln task 1

---
+++ 03.CSharpDev/01.Advanced/ExamPreparations/25June2017/01.Regeh/Regeh.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class Regeh
+{
+    public static void Main()
+    {
+        var pattern = @"\[[^\r\n\t\f\v \[]+?[<]([0-9]+)REGEH([0-9]+)[>][^\r\n\t\f\v \]]+?\]";
+        Regex reg = new Regex(pattern);
+        string input = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+        var matches = reg.Matches(input);
+        int index = 0;
+
+        foreach (Match match in matches)
+        {
+            index += int.Parse(match.Groups[1].ToString());
+
+            if (index > input.Length - 1)
+            {
+                index = index % (input.Length - 1);
+            }
+
+            sb.Append(input[index]);
+
+            index += int.Parse(match.Groups[2].ToString());
+
+            if (index > input.Length - 1)
+            {
+                index = index % (input.Length - 1);
+            }
+
+            sb.Append(input[index]);
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
