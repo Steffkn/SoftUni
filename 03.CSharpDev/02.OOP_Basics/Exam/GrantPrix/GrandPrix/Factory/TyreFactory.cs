93e495d72a5238bf174b59f66df20649d4172d1e
Steffkn <steff.kn@abv.bg>
2018-03-17 21:45:41 +0200

    Added: Exam GrandPrix

---
+++ 03.CSharpDev/02.OOP_Basics/Exam/GrantPrix/GrandPrix/Factory/TyreFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TyreFactory
+{
+    public Tyre GetTyre(List<string> commandArgs)
+    {
+        string tyreType = commandArgs[4];
+        var tyreHardness = double.Parse(commandArgs[5]);
+
+        switch (tyreType)
+        {
+            case "Hard":
+                return new HardTyre(tyreHardness);
+            case "Ultrasoft":
+                var tyreGrip = double.Parse(commandArgs[6]);
+                return new UltrasoftTyre(tyreHardness, tyreGrip);
+            default:
+                throw new ArgumentException();
+        }
+    }
+}
---
+++ 03.CSharpDev/02.OOP_Basics/Exam/GrantPrix/GrandPrix/Models/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Car
+{
+    private const double maxFuel = 160;
+    private double fuelAmount;
+
+    public Car(int hp, double fuelAmount, Tyre tyre)
+    {
+        this.Hp = hp;
+        this.FuelAmount = fuelAmount;
+        this.Tyre = tyre;
+    }
+
+    public int Hp { get; }
+
+    public double FuelAmount
+    {
+        get => fuelAmount;
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Out of fuel");
+            }
+
+            this.fuelAmount = Math.Min(value, maxFuel);
+        }
+    }
+
+    public Tyre Tyre { get; private set; }
+
+    public void SetTyre(Tyre tyre)
+    {
+        this.Tyre = tyre;
+    }
+
+    public void Refuel(double amount)
+    {
+        this.FuelAmount += amount;
+    }
+
+    public void BurnFuel(double amount)
+    {
+        this.FuelAmount -= amount;
+    }
+}
