7a13064fa06f3a3b34f5c1aa05c289ae249899aa
Stefan <steff.kn@abv.bg>
2019-11-12 08:15:39 +0200

    added Entity Relations HW

---
+++ 04.CSharpDB/02.EntityFrameworkCore/05.EntityRelations/P01_StudentSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_StudentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
