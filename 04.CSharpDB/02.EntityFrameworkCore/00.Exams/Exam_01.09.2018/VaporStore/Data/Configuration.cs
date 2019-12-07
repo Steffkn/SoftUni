df1660e86a0bb5ae4a394d39f484bd923bc71839
Stefan <steff.kn@abv.bg>
2019-12-07 20:59:06 +0200

    Removed older exams

--- 04.CSharpDB/02.EntityFrameworkCore/00.Exams/Exam_01.09.2018/VaporStore/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VaporStore.Data
-{
-	public static class Configuration
-	{
-		public static string ConnectionString =
-            @"Server=DESKTOP-AGCLSI5\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
-	}
-}
--- 04.CSharpDB/02.EntityFrameworkCore/00.Exams/Exam_12.08.2018/SoftJail/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoftJail.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-AGCLSI5\SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
-    }
-}
