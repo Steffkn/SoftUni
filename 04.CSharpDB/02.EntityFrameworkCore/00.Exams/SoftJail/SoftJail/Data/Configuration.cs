8c7a070fbf5314e8a0805efb4e5b7d3ecacc29a2
Stefan <steff.kn@abv.bg>
2019-12-07 21:05:07 +0200

    Moved exam SoftJail

--- 04.CSharpDB/02.EntityFrameworkCore/00.Exams/SoftJail/SoftJail/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoftJail.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=(localdb)\MSSQLlocalDB;Database=SoftJail;Trusted_Connection=True";
-    }
-}
